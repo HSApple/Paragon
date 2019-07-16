0f3b455b3e0bb4e7d7364279b62dd7ed8807e0f6
jimenjor117 <jj854528@gmail.com>
2019-07-16 12:40:49 -0500

    hello there

---
+++ S.O.S/StressAnswer2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StressAnswer2ViewController.swift
+//  S.O.S
+//
+//  Created by student on 7/16/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class StressAnswer2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ S.O.S/StressAnswerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StressAnswerViewController.swift
+//  S.O.S
+//
+//  Created by student on 7/16/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class StressAnswerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
